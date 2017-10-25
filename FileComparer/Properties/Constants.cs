d1d2ea920565945115640ac22772954323faf21a
Mark Khromov <lane.71rus@gmail.com>
2017-10-25 10:30:28 +0300

    Improve assembly info

---
+++ FileComparer/Properties/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace FileComparer.Properties {
+    static class Constants {
+        public const string Version = "1.0.0.0";
+        public const string Title = "File Comparer";
+        public const string Description = "";
+        public const string Copyright = "Copyright ©  2017";
+    }
+}
