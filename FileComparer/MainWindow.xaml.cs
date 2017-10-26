253246fe1737ba5e0756036efe496f74e5905cab
Ganningorlightman <ganning@mail.ru>
2017-10-26 22:30:10 +0300

    Add button

--- FileComparer/MainWindow.xaml.cs
+++ FileComparer/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace FileComparer
 {
@@ -23,6 +25,33 @@ namespace FileComparer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "txt files All files (*.*)|*.*";
+            if (dialog.ShowDialog() == true)
+            {
+                string filename = dialog.FileName;
+                //label1.Text = filename.Split('\\')[filename.Split('\\').Length - 1];
+                using (var sr = new StreamReader(filename))
+                {
+                    //string line;
+                    //while ((line = sr.ReadLine()) != null)
+                    //{
+                       
+                    //}
+                }
+            }
+            else
+            {
+                //label1.Text = "Файл не выбран";
+            }   
+        }
+        public string getFileExtension(string fileName)
+        {
+            return fileName.Substring(fileName.LastIndexOf(".") + 1);
         }
     }
 }
