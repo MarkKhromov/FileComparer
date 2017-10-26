61ed92d7fd02a9b5ef67036266bc74f11623fc5f
Mark Khromov <lane.71rus@gmail.com>
2017-10-27 02:11:07 +0300

    Improve main window

--- FileComparer/MainWindow.xaml.cs
+++ FileComparer/MainWindow.xaml.cs
@@ -1,31 +1,12 @@
-﻿using System.IO;
-using System.Windows;
-using Microsoft.Win32;
+﻿using System.Windows;
 
 namespace FileComparer {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            DataContext = new Command(LoadFile, () => true);
+            DataContext = new MainWindowViewModel(this);
         }
 
-        void LoadFile() {
-            var dialog = new OpenFileDialog();
-            dialog.Filter = FilterStringBuilder.Build(FileFormats.Txt, FileFormats.All);
-            if(dialog.ShowDialog(this) == true) {
-                string filename = dialog.FileName;
-                //label1.Text = filename.Split('\\')[filename.Split('\\').Length - 1];
-                using(var sr = new StreamReader(filename)) {
-                    //string line;
-                    //while ((line = sr.ReadLine()) != null)
-                    //{
-
-                    //}
-                }
-            } else {
-                //label1.Text = "Файл не выбран";
-            }
-        }
         public string GetFileExtension(string fileName) {
             return fileName.Substring(fileName.LastIndexOf(".") + 1);
         }
---
+++ FileComparer/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Windows.Input;
+using Microsoft.Win32;
+
+namespace FileComparer {
+    class MainWindowViewModel : INotifyPropertyChanged {
+        public MainWindowViewModel(MainWindow mainWindow) {
+            this.mainWindow = mainWindow;
+            loadFileCommand = new Command(LoadFile, () => true);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        readonly MainWindow mainWindow;
+
+        readonly ICommand loadFileCommand;
+        public ICommand LoadFileCommand { get { return loadFileCommand; } }
+        string fileName;
+        public string FileName {
+            get { return fileName; }
+            set {
+                fileName = value;
+                RaisePropertyChanged(nameof(FileName));
+            }
+        }
+
+        void LoadFile() {
+            var dialog = new OpenFileDialog {
+                Filter = FilterStringBuilder.Build(FileFormats.Txt, FileFormats.All)
+            };
+            if(dialog.ShowDialog(mainWindow) == true) {
+                FileName = dialog.FileName;
+                //label1.Text = filename.Split('\\')[filename.Split('\\').Length - 1];
+                using(var sr = new StreamReader(FileName)) {
+                    //string line;
+                    //while ((line = sr.ReadLine()) != null)
+                    //{
+
+                    //}
+                }
+            } else {
+                //label1.Text = "Файл не выбран";
+            }
+        }
+
+        void RaisePropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
