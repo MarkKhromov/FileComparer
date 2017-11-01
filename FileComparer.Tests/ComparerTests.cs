1c1f0a9805c0dc4868042dabca1e93a78a2aed48
Mark Khromov <lane.71rus@gmail.com>
2017-11-01 12:09:49 +0300

    Added comparer tests

---
+++ FileComparer.Tests/ComparerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FileComparer.Tests {
+    [TestFixture]
+    public class ComparerTests {
+        [Test]
+        public void GetTest() {
+            foreach(FileFormats fileFormat in Enum.GetValues(typeof(FileFormats))) {
+                Comparer.Get(fileFormat);
+            }
+        }
+    }
+}
