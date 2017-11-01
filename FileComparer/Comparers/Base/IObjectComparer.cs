57b64225d7cc94d61568c4cd0a5d7c5b7a6843cf
Mark Khromov <lane.71rus@gmail.com>
2017-11-01 11:55:24 +0300

    Added bytes and text comparers

---
+++ FileComparer/Comparers/Base/IObjectComparer.cs
@@ -0,0 +1,7 @@
+﻿namespace FileComparer.Comparers.Base {
+    public interface IObjectComparer {
+        bool Compare(object object1, object object2);
+        object GetObject(string fileName);
+
+    }
+}
