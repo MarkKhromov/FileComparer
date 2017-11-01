c688e3ef0fc800f99e8d12668b8c07e7ce6ef4d2
Mark Khromov <lane.71rus@gmail.com>
2017-11-01 12:04:24 +0300

    Added comparer accessor

---
+++ FileComparer/Comparer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using FileComparer.Comparers;
+using FileComparer.Comparers.Base;
+
+namespace FileComparer {
+    public static  class Comparer {
+        static Comparer() {
+            comparers = new Dictionary<FileFormats, IObjectComparer>();
+            Register(FileFormats.Txt, new TextComparer());
+            Register(FileFormats.All, new BytesComparer());
+        }
+
+        static readonly IDictionary<FileFormats, IObjectComparer> comparers;
+
+        public static IObjectComparer Get(FileFormats fileFormat) {
+            return comparers[fileFormat];
+        }
+
+        static void Register(FileFormats fileFormat, IObjectComparer objectComparer) {
+            comparers.Add(fileFormat, objectComparer);
+        }
+    }
+}
